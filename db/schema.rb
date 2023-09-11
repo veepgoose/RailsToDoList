dd09080221a6c86655fd609576c959037ceffeac
Goose <victoriaplatt@loveplanes.co.uk>
2023-09-11 10:25:02 +0100

    Created views for todo list and todo items, tested and working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_142251) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_085301) do
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_142251) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
