f64610ace22be5dc96b3dfb51d08ccf8820bc818
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-09-11 09:44:15 +0100

    Created To-Do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :todo_lists
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "todo_lists#index"
 end
